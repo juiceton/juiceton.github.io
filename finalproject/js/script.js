0bb5942c054929d6a05fcee7c2797bd8a97a4e73
juiceton <justin.cruz@me.com>
2016-07-14 13:15:34 -0400

    Initial upload

---
+++ finalproject/js/script.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+  $("#container1").twentytwenty();
+});
+
+$(window).load(function() {
+  $("#container2").twentytwenty();
+});
+
+$(window).load(function() {
+  $("#container3").twentytwenty();
+});
