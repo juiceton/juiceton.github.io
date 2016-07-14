2fea458334da77aefc606060198134fec01b4f7c
juiceton <justin.cruz@me.com>
2016-07-14 13:41:23 -0400

    Fix jQuery

--- finalproject/js/script.js
+++ finalproject/js/script.js
@@ -1,11 +1,11 @@
-$(window).load(function() {
-  $("#container1").twentytwenty();
+jQuery(window).load(function() {
+  jQuery("#container1").twentytwenty();
 });
 
-$(window).load(function() {
-  $("#container2").twentytwenty();
+jQuery(window).load(function() {
+  jQuery("#container2").twentytwenty();
 });
 
-$(window).load(function() {
-  $("#container3").twentytwenty();
+jQuery(window).load(function() {
+  jQuery("#container3").twentytwenty();
 });
